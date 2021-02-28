f6811cdf9fbfd9d171a43d81a59372fa7ac4fadc
D-0000000000 <D_0000000000@hotmail.com>
2021-02-28 19:43:36 +0800

    Add support for picture

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-// 注意: 本项目的所有源文件都必须是 UTF-8 编码
-
-// 这是一个“反撤回”机器人
-// 在群里回复 “/anti-recall enabled.” 或者 “撤回没用” 之后
-// 如果有人在群里撤回，那么机器人会把撤回的内容再发出来
-
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <map>
+#include <ctime>
 #include <mirai.h>
 #include "myheader.h"
 
@@ -24,22 +20,74 @@ uint64_t getull(std::string &str)
 	return ret;
 }
 
+bool getPic(std::vector<std::string> &url, std::string &curTime)
+{
+	std::string curPath = std::filesystem::current_path();
+	if (!std::filesystem::exists(curPath + "/cache/"))
+	{
+		std::filesystem::create_directory(curPath + "/cache/");
+	}
+	if (!std::filesystem::exists(curPath + "/cache/" + curTime))
+	{
+		std::filesystem::create_directory(curPath + "/cache/" + curTime);
+	}
+	std::filesystem::rename("./msgTitle.txt", "./cache/" + curTime + "/msgTitle.txt");
+	std::filesystem::rename("./msgBody.txt", "./cache/" + curTime + "/msgBody.txt");
+	std::filesystem::rename("./msgURL.txt", "./cache/" + curTime + "/msgURL.txt");
+	std::filesystem::rename("./msgPicURL.txt", "./cache/" + curTime + "/msgPicURL.txt");
+	if (url.size() == 0)
+	{
+		std::filesystem::remove("./ cache / " + curTime + " / msgPicURL.txt");
+		return false;
+	}
+	for (auto &picurl : url)
+	{
+		std::string fileName = "";
+		int start = picurl.size() - 1;
+		for (auto i = start; i >= 0; i--)
+		{
+			if (picurl[i] == '/')
+			{
+				start = i + 1;
+				break;
+			}
+		}
+		for (int i = start; i < picurl.size(); i++)
+		{
+			fileName += picurl[i];
+		}
+		// std::string curlcmd = "curl -o" + curPath + "/cache/" + curTime + "/" + fileName + picurl;
+		std::string curlcmd = "curl " + picurl + " -o ./cache/" + curTime + "/" + fileName;
+		system(curlcmd.c_str());
+		std::cout << curlcmd << std::endl;
+		picurl = "./cache/" + curTime + "/" + fileName;
+	}
+	return true;
+}
+
+bool AddPictures(Cyan::MiraiBot &bot, std::vector<std::string> &picCache, Cyan::MessageChain &msg)
+{
+	int count = 0;
+	if (picCache.size() == 0)
+	{
+		return false;
+	}
+	for (auto cache : picCache)
+	{
+		count++;
+		Cyan::GroupImage img = bot.UploadGroupImage(cache);
+		msg = msg + Cyan::MessageChain().Image(img);
+	}
+	std::cout << count << std::endl;
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
 	using namespace std;
 	using namespace Cyan;
 
-#if defined(WIN32) || defined(_WIN32)
-	// 切换代码页，让 CMD 可以显示 UTF-8 字符
-	system("chcp 65001");
-#endif
-
-	if (argc != 2)
-	{
-		std::cout << "WRONG ARGS" << std::endl;
-		return 0;
-	}
-
+	std::string currentTime = std::to_string(time(0));
 	std::string authKey = "";
 	std::string QQstr = "";
 	std::string Gstr = "";
@@ -90,10 +138,61 @@ int main(int argc, char *argv[])
 	}
 	cout << "Bot Working..." << endl;
 
-	std::string message = argv[1];
-	Cyan::MessageChain msg = MessageChain().Plain(message);
-	bot.SendMessage(GQQ, msg);
-	std::cout << "Complete" << std::endl;
+	std::string title = "", body = "", url = "";
+	std::fstream msgFile;
+	std::stringstream sstp;
+	msgFile.open("msgTitle.txt");
+	sstp << msgFile.rdbuf();
+	title = sstp.str();
+	sstp.clear();
+	sstp.str("");
+	msgFile.close();
+
+	msgFile.open("msgBody.txt");
+	sstp << msgFile.rdbuf();
+	body = sstp.str();
+	sstp.clear();
+	sstp.str("");
+	msgFile.close();
+
+	msgFile.open("msgURL.txt");
+	sstp << msgFile.rdbuf();
+	url = sstp.str();
+	sstp.clear();
+	sstp.str("");
+	msgFile.close();
+
+	msgFile.open("msgPicURL.txt");
+	std::vector<std::string> picurl;
+	std::string strtp;
+	while (msgFile >> strtp)
+	{
+		picurl.push_back(strtp);
+	}
+	msgFile.close();
+
+	if (title.empty() || body.empty() || url.empty())
+	{
+		std::cout << "Invalid message" << std::endl;
+		return 0;
+	}
+
+	getPic(picurl, currentTime);
+	try
+	{
+		Cyan::MessageChain msg = MessageChain().Plain(title + "\n" + body + "\n" + url + "\n");
+		if (picurl.size() != 0)
+		{
+			AddPictures(bot, picurl, msg);
+		}
+		bot.SendMessage(GQQ, msg);
+		std::cout << "Complete" << std::endl;
+	}
+	catch (const std::exception &ex)
+	{
+		std::cerr << "Fail to send message" << '\n';
+		std::cerr << ex.what() << '\n';
+	}
 
 	return 0;
 }
